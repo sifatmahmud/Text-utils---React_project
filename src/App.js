7e3ec6021490195f08b1ec421f0486178487d670
Sifat Mahmud <sifatmahmud5293@gmail.com>
2021-11-14 16:00:51 +0600

    create a about.js file in conponents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Textform from "./Components/Textform";
+// import About from "./Components/About";
 
 function App() {
   return (
     <>
       <Navbar title="Textutils" />
       <Textform heading="Enter text here to analyze" />
+      {/* <About /> */}
     </>
   );
 }
